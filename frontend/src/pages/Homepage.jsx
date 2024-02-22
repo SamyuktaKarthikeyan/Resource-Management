67b38f863a24f47c24cf656bdaca048401f6883f
SamyuktaKarthikeyan <727721eucs129@skcet.ac.in>
2024-02-22 20:29:40 +0530

    first page skeleton

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,17 @@
+import Herohome from '../components/Herohome'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Features from '../components/Features'
+
+const Homepage = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Herohome/>
+        <Features/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Homepage
